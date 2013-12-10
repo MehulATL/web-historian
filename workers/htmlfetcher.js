bf5aa3d297944bd20bcad41acf4f14573d69e7e2
Hack Reactor Students <mehul8@gmail.com>
2013-12-09 20:11:21 -0800

    sudo-coded htmlfetcher

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,23 @@
-// eventually, you'll have some code here that uses the tested helpers 
+// eventually, you'll have some code here that uses the tested helpers
 // to actually download the urls you want to download.
+var http = require('http-request');
+var fs = require('fs');
+
+// load sites.txt
+// grab url from each line
+// remove from file
+// download and save website to unique text file.
+
+http.get({
+  url: 'http://www.google.com',
+  progress: function (current, total) {
+    console.log('downloaded %d bytes from %d', current, total);
+  }
+}, 'get.bin', function (err, res) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(res.code, res.headers, res.file);
+});
