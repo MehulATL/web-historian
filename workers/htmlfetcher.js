80460cf4321a3e7dd886b07ada10fc20f5f3da87
Hack Reactor Students <mehul8@gmail.com>
2013-12-10 11:42:05 -0800

    cronjobs somewhat working.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -5,21 +5,9 @@ var fs = require('fs');
 var fetcher = require('./lib/html-fetcher-helpers');
 var filename = __dirname + '/../data/sites.txt';
 
-
+console.log('path', __dirname);
 
 fetcher.readUrls(filename, fetcher.downloadUrls);
 
-
-// http.get({
-//   url: 'http://www.google.com',
-//   progress: function (current, total) {
-//     console.log('downloaded %d bytes from %d', current, total);
-//   }
-// }, 'get.bin', function (err, res) {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-
-//   console.log(res.code, res.headers, res.file);
-// });
+// CRONJOB
+// * * * * * /opt/boxen/nodenv/versions/v0.10.0/bin/node /Users/hackreactor/code/lovenick/2013-11-web-historian/workers/htmlfetcher.js >/tmp/stdout.log 2>~/Desktop/stderr.log
