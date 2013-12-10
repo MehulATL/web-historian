fd818e4cf414c868756ded28f76229a9f44dd781
Nick Loveridge <nick@fusemedia.ca>
2013-12-10 09:58:43 -0800

    pushing update to pull down on HR comp

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -2,22 +2,57 @@
 // to actually download the urls you want to download.
 var http = require('http-request');
 var fs = require('fs');
+var filename = __dirname + '/../data/sites.txt';
+
+
+
+
+// grab url from each line
+var getUrls = function(data){
+  var urls = data.split('\n');
+  urls.pop();
+
+  for(var i = 0; i < urls.length; i++) {
+    // load url
+    // save html to file.
+  }
+
+  // erase file.
+
+}
+
+var loadFileHelper = function(filename, callBack) {
+  fs.readFile(filename, function(error, data){
+    if (error){
+      console.log('Error loading sites.txt', error);
+      // callBack(response, 'error', 404);
+    } else {
+      // console.log(data.toString());
+      getUrls(data.toString());
+    }
+  });
+};
+
+
+
+
+// http.get({
+//   url: 'http://www.google.com',
+//   progress: function (current, total) {
+//     console.log('downloaded %d bytes from %d', current, total);
+//   }
+// }, 'get.bin', function (err, res) {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+
+//   console.log(res.code, res.headers, res.file);
+// });
+
 
 // load sites.txt
-// grab url from each line
+loadFileHelper(filename);
+
 // remove from file
 // download and save website to unique text file.
-
-http.get({
-  url: 'http://www.google.com',
-  progress: function (current, total) {
-    console.log('downloaded %d bytes from %d', current, total);
-  }
-}, 'get.bin', function (err, res) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  console.log(res.code, res.headers, res.file);
-});
